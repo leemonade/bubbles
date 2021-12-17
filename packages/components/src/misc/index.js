41ab623801a891c499d3aa9b6f162a5422e3951d
Albert <albert@iMac-de-Yomyer.local>
2021-12-17 15:23:27 +0100

    (LMVP-35) - Alert: Component moved from misc to feedback

--- packages/components/src/misc/index.js
+++ packages/components/src/misc/index.js
@@ -1,6 +1,5 @@
 import { Logo } from './Logo';
 import { ImageLoader } from './ImageLoader';
 import { InlineSvg } from './InlineSvg';
-import { Alert } from './Alert';
 
-export { Logo, ImageLoader, InlineSvg, Alert };
+export { Logo, ImageLoader, InlineSvg };
