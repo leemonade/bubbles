cc963eee9da213a5f492db696da94567202b88a2
Jaime Gómez Cimarro <cerberupo@gmail.com>
2021-12-22 08:39:38 +0100

    Fixed TextInput not recieving value, onChange, onBlur, name props

--- packages/components/src/form/TextInput/TextInput.js
+++ packages/components/src/form/TextInput/TextInput.js
@@ -2,23 +2,29 @@ import React, { forwardRef } from 'react';
 import { isNil } from 'lodash';
 import { useId } from '@mantine/hooks';
 import { Input } from '../Input';
-import { InputWrapper, INPUT_WRAPPER_ORIENTATION, INPUT_WRAPPER_SIZES } from '../InputWrapper';
+import { INPUT_WRAPPER_ORIENTATION, INPUT_WRAPPER_SIZES, InputWrapper } from '../InputWrapper';
 
 export const TEXT_INPUT_SIZES = INPUT_WRAPPER_SIZES;
 export const TEXT_INPUT_ORIENTATION = INPUT_WRAPPER_ORIENTATION;
 
-export const TextInput = forwardRef(({ error, size, placeholder, rightSection, ...props }, ref) => {
-  const uuid = useId();
-  return (
-    <InputWrapper {...props} uuid={uuid} size={size} error={error}>
-      <Input
-        id={uuid}
-        ref={ref}
-        size={size}
-        placeholder={placeholder}
-        rightSection={rightSection}
-        invalid={!isNil(error) && error != ''}
-      />
-    </InputWrapper>
-  );
-});
+export const TextInput = forwardRef(
+  ({ error, size, placeholder, rightSection, name, value, onBlur, onChange, ...props }, ref) => {
+    const uuid = useId();
+    return (
+      <InputWrapper {...props} uuid={uuid} size={size} error={error}>
+        <Input
+          id={uuid}
+          ref={ref}
+          size={size}
+          name={name}
+          onBlur={onBlur}
+          onChange={onChange}
+          value={value}
+          placeholder={placeholder}
+          rightSection={rightSection}
+          invalid={!isNil(error) && error != ''}
+        />
+      </InputWrapper>
+    );
+  }
+);
