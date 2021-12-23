2781e42cf872d112d1b05156c8a32a5f9f194a8e
Johan Sosa <sosa.johan@gmail.com>
2021-12-23 10:52:46 +0100

    HOTFIX: Add component prefix to every constant export

--- packages/components/scripts/templates/mantine/__name__.js
+++ packages/components/scripts/templates/mantine/__name__.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 import { __mantine__ as Mantine__mantine__ } from '@mantine/core';
 import { __name__Styles } from './__name__.styles';
 
-export const DEFAULT_PROPS = {};
+export const __name__(constantCase)_DEFAULT_PROPS = {};
+export const __name__(constantCase)_PROP_TYPES = {};
 
 const __name__ = forwardRef(({ ...props }, ref) => {
   const { classes, cx } = __name__Styles({});
@@ -11,10 +12,8 @@ const __name__ = forwardRef(({ ...props }, ref) => {
   return <Mantine__mantine__ {...props} ref={ref} classNames={classes} />;
 });
 
-__name__.defaultProps = DEFAULT_PROPS;
+__name__.defaultProps = __name__(constantCase)_DEFAULT_PROPS;
 
-__name__.propTypes = {
-  //
-};
+__name__.propTypes = __name__(constantCase)_PROP_TYPES;
 
 export { __name__ };
