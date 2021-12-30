2bc1a2ca441336ad2a1e66b31ab4eb1a3b707b42
Albert Araque Casaus <albert.araque.casaus@gmail.com>
2021-12-30 16:32:17 +0100

    (LMVP-80) - Pager: Component with styles and GoTo

---
+++ packages/components/src/navigation/Pager/Pager.styles.js
@@ -0,0 +1,72 @@
+import { createStyles } from '@mantine/styles';
+import { pxToRem, getPaddings, getFontExpressive, getFontProductive } from '../../theme.mixins';
+
+export const PagerStyles = createStyles(
+  (theme, { withGoTo, withControls, withEdges, disabled }) => {
+    return {
+      root: {
+        gap: 0,
+        marginLeft: withGoTo && !withControls ? !withEdges && pxToRem(8) : null,
+        'button:hover': {
+          backgroundColor: disabled ? 'none' : theme.colors.ui03,
+        },
+        'button:active:not(:disabled):not(.mantine-ref_dots_1)': {
+          transform: disabled && 'none',
+        },
+      },
+      item: {
+        ...getFontExpressive(theme.fontSizes['2'], 400),
+        color: disabled ? theme.colors.text05 : theme.colors.text01,
+        backgroundColor: theme.colors.mainWhite,
+        border: 'none',
+        borderRadius: '50%',
+        height: pxToRem(48),
+        width: pxToRem(48),
+        maxWidth: pxToRem(48),
+        padding: 0,
+        lineHeight: pxToRem(24),
+      },
+      active: {
+        fontWeight: !disabled && 700,
+        color: disabled ? theme.colors.text05 : theme.colors.text01,
+        backgroundColor: theme.colors.mainWhite,
+        border: disabled ? 'none' : `1px solid ${theme.colors.ui01}`,
+        borderRadius: '50%',
+      },
+      dots: {
+        color: theme.colors.text05,
+        transform: `translateY(${pxToRem(4)})`,
+      },
+      container: {
+        display: 'flex',
+      },
+      goto: {
+        span: {
+          ...getFontProductive(theme.fontSizes['2'], 500),
+        },
+        input: {
+          ...getFontProductive(theme.fontSizes['2'], 500),
+          width: pxToRem(60),
+          height: pxToRem(48),
+          textAlign: 'center',
+          borderRadius: pxToRem(4),
+          border: `1px solid ${theme.colors.ui01}`,
+          padding: 0,
+          '&:focus': {
+            outline: 'none',
+          },
+          '&::-webkit-inner-spin-button, &::-webkit-outer-spin-button': {
+            appearance: 'none',
+            margin: 0,
+          },
+          '&[type=number]': {
+            appearance: 'textfield',
+          },
+        },
+        display: 'flex',
+        alignItems: 'center',
+        gap: pxToRem(8),
+      },
+    };
+  }
+);
