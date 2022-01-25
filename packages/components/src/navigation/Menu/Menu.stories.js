d6396a1345ce488bc73e88ef425256ac8ad858bf
Albert Araque Casaus <albert.araque.casaus@gmail.com>
2022-01-25 13:02:29 +0100

    (LMVP-132) - Menu: Component with custom shadows

---
+++ packages/components/src/navigation/Menu/Menu.stories.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Menu, MENU_DEFAULT_PROPS, MENU_POSITIONS, MENU_PLACEMENT } from './Menu';
+import mdx from './Menu.mdx';
+import { StarIcon, DeleteBinIcon, FlagIcon } from '@bubbles-ui/icons/solid/';
+import { Stack } from '../../layout/Stack';
+
+export default {
+  title: 'Atoms/Navigation/Menu',
+  parameters: {
+    component: Menu,
+    docs: {
+      page: mdx,
+    },
+    design: {
+      type: 'figma',
+      // url: 'https://www.figma.com/file/kcSXz3QZFByFDTumNgzPpV/?node-id=2962%3A31342',
+    },
+  },
+  argTypes: {
+    position: { options: MENU_POSITIONS, type: 'select' },
+    placement: { options: MENU_PLACEMENT, type: 'select' },
+    gutter: { control: 'number' },
+    withArrow: { control: 'boolean' },
+  },
+};
+
+const Template = ({ children, ...props }) => {
+  return (
+    <Stack fullWidth style={{ height: '100vh' }} alignItems={'center'} justifyContent={'center'}>
+      <Menu {...props}>{children}</Menu>
+    </Stack>
+  );
+};
+
+export const Playground = Template.bind({});
+
+Playground.args = {
+  ...MENU_DEFAULT_PROPS,
+  items: [
+    {
+      icon: <StarIcon />,
+      children: 'Item 1',
+      onClick: () => alert('Item 1 clicked'),
+    },
+    {
+      icon: <DeleteBinIcon />,
+      children: 'Item 2',
+      onClick: () => alert('Item 1 clicked'),
+    },
+    {
+      icon: <FlagIcon />,
+      children: 'Item 3',
+      onClick: () => alert('Item 3 clicked'),
+    },
+  ],
+};
