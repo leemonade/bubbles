e1685056a0be74b0ad9906891624d5e32a3589c3
Albert Araque Casaus <albert.araque.casaus@gmail.com>
2022-01-26 11:42:57 +0100

    (LMVP-122) - ProgramRules: Fixed errors after merge

--- packages/components/src/navigation/Menu/Menu.styles.js
+++ packages/components/src/navigation/Menu/Menu.styles.js
@@ -1,11 +1,6 @@
 import { createStyles } from '@mantine/styles';
 import { pxToRem, getPaddings, getFontExpressive, getFontProductive } from '../../theme.mixins';
 
-<<<<<<< HEAD:packages/components/src/navigation/Stepper/Stepper.styles.js
-export const StepperStyles = createStyles((theme, {}) => {
-  return {
-    root: {},
-=======
 export const MenuStyles = createStyles((theme, {}) => {
   return {
     root: {
@@ -14,6 +9,5 @@ export const MenuStyles = createStyles((theme, {}) => {
     body: {
       boxShadow: theme.shadows.shadow03,
     },
->>>>>>> Menu:packages/components/src/navigation/Menu/Menu.styles.js
   };
 });
