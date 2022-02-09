7c0acdbb7cf30ec2c7e6bfed387e016e824319a1
Miguel Ángel Florido Cifuentes <miguel.mangel.ez11@gmail.com>
2022-02-09 12:51:18 +0100

    Added linkAll script

---
+++ scripts/link.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+const childProcess = require('child_process');
+
+async function main() {
+  const dir = path.resolve(__dirname, '../packages');
+
+  const directories = fs.readdirSync(dir, { withFileTypes: true });
+
+  const result = await Promise.all(
+    directories
+      .filter((directory) => directory.isDirectory())
+      .map((directory) => {
+        return new Promise((resolve) => {
+          // Build the package with child process
+          const packagePath = path.resolve(dir, directory.name);
+          const name = path.basename(packagePath);
+
+          childProcess
+            .exec(`yarn --cwd ${packagePath} link`)
+            // on finish
+            .on('message', (message) => {
+              console.log(`[${name}]`, message);
+            })
+            .on('exit', (code) => {
+              if (code !== 0) {
+                console.error(`Error linking ${name}`);
+                process.exit(1);
+              } else {
+                console.log(`Linked ${name}`);
+              }
+              resolve(name);
+            });
+        });
+      })
+  );
+
+  console.log();
+  console.log(' --- RUN THE FOLLOWING COMMAND IN YOUR PROJECT ---');
+  console.log();
+
+  console.log(`yarn link  @bubbles-ui/${result.join(' @bubbles-ui/')}`);
+  console.log();
+  console.log();
+}
+
+main();
