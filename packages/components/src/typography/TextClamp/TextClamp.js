b4a270873c322f4035e6afef33981e24a701d9c1
Johan Sosa <sosa.johan@gmail.com>
2022-02-14 11:44:15 +0100

    ADDED: Text truncated prop CREATED: TextClamp lines

---
+++ packages/components/src/typography/TextClamp/TextClamp.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Clamp from 'react-multiline-clamp';
+import { Button } from '../../form';
+
+export const TEXT_CLAMP_DEFAULT_PROPS = {
+  lines: 2,
+  maxLines: 8,
+  withTooltip: true,
+  withToggle: false,
+  showMore: 'More',
+  showLess: 'Less',
+  onShowMore: () => {},
+};
+export const TEXT_CLAMP_PROP_TYPES = {
+  lines: PropTypes.number,
+  maxLines: PropTypes.number,
+  withTooltip: PropTypes.bool,
+  withToggle: PropTypes.bool,
+  showMore: PropTypes.string,
+  showLess: PropTypes.string,
+  onShowMore: PropTypes.func,
+};
+
+const TextClamp = ({
+  children,
+  lines,
+  maxLines,
+  withTooltip,
+  withToggle,
+  showMore,
+  showLess,
+  onShowMore,
+  ...props
+}) => {
+  return (
+    <Clamp
+      {...props}
+      lines={lines}
+      maxLines={maxLines}
+      withTooltip={withTooltip}
+      withToggle={withToggle}
+      showMoreElement={({ toggle }) => (
+        <Button size="xs" compact variant="link" onClick={toggle}>
+          {showMore}
+        </Button>
+      )}
+      showLessElement={({ toggle }) => (
+        <Button size="xs" compact variant="link" onClick={toggle}>
+          {showLess}
+        </Button>
+      )}
+      onShowMore={onShowMore}
+    >
+      {children}
+    </Clamp>
+  );
+};
+
+TextClamp.defaultProps = TEXT_CLAMP_DEFAULT_PROPS;
+TextClamp.propTypes = TEXT_CLAMP_PROP_TYPES;
+
+export { TextClamp };
