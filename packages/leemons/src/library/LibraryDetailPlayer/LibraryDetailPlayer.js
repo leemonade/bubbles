5c308611d37a233bab1944f15f6fba40473d79e5
Albert Araque Casaus <albert.araque.casaus@gmail.com>
2022-02-16 13:16:40 +0100

    (LMVP-138) - LibraryDetailPlayer: Styles and functionality of progress bar

--- packages/leemons/src/library/LibraryDetailPlayer/LibraryDetailPlayer.js
+++ packages/leemons/src/library/LibraryDetailPlayer/LibraryDetailPlayer.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo, useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 import {
   Box,
@@ -55,10 +55,13 @@ const LibraryDetailPlayer = ({
   fileType,
   ...props
 }) => {
+  const playerRef = useRef(null);
   const [showPlayer, setShowPlayer] = useState(false);
   const [isPlaying, setIsPlaying] = useState(false);
   const [played, setPlayed] = useState(0);
   const [seconds, setSeconds] = useState(0);
+  const [seeking, setSeeking] = useState(false);
+  const [seekValue, setSeekValue] = useState(0);
 
   const playedPercentage = useMemo(() => played * 100, [played]);
 
@@ -72,6 +75,20 @@ const LibraryDetailPlayer = ({
       setSeconds(elapsedSeconds);
     }
     setPlayed(played);
+    if (!seeking) setSeekValue(played);
+  };
+
+  const handleSeekChange = (e) => {
+    setSeekValue(e.target.value);
+  };
+
+  const handleSeekMouseDown = (e) => {
+    setSeeking(true);
+  };
+
+  const handleSeekMouseUp = (e) => {
+    setSeeking(false);
+    playerRef.current.seekTo(parseFloat(seekValue));
   };
 
   const icon = useMemo(
@@ -87,82 +104,90 @@ const LibraryDetailPlayer = ({
     { name: 'LibraryDetailPlayer' }
   );
   return (
-    <Box className={classes.root}>
-      <Box className={classes.coverWrapper}>
-        <Box className={classes.reactPlayerWrapper}>
-          <ReactPlayer
-            url={url}
-            width="100%"
-            height="100%"
-            playing={isPlaying}
-            onProgress={({ played, playedSeconds }) => handleOnProgress(played, playedSeconds)}
-            onSeek={(seconds) => console.log('seek', seconds)}
-            progressInterval={100}
-            className={classes.reactPlayer}
-          />
-          {fileType === 'audio' && (
-            <Box className={classes.audioIcon}>
-              <FileIcon fileType={fileType} size={64} color={'#FFF'} />
+    <>
+      <Box className={classes.root}>
+        <Box className={classes.coverWrapper}>
+          <Box className={classes.reactPlayerWrapper}>
+            <ReactPlayer
+              ref={playerRef}
+              url={url}
+              width="100%"
+              height="100%"
+              playing={isPlaying}
+              onProgress={({ played, playedSeconds }) => handleOnProgress(played, playedSeconds)}
+              progressInterval={100}
+              className={classes.reactPlayer}
+            />
+            {fileType === 'audio' && (
+              <Box className={classes.audioIcon}>
+                <FileIcon fileType={fileType} size={64} color={'#FFF'} />
+              </Box>
+            )}
+            <Box className={classes.progressBarWrapper}>
+              <Box className={classes.progressBar}>
+                <Box
+                  className={classes.progressBarValue}
+                  style={{
+                    width: playedPercentage + '%',
+                  }}
+                />
+                <input
+                  className={classes.seekSlider}
+                  type={'range'}
+                  min={0}
+                  max={0.999999}
+                  step={'any'}
+                  value={seekValue}
+                  onChange={handleSeekChange}
+                  onMouseDown={handleSeekMouseDown}
+                  onMouseUp={handleSeekMouseUp}
+                />
+              </Box>
+
+              <Text size={'xs'} role={'productive'} className={classes.duration}>
+                {getDuration()}
+              </Text>
             </Box>
-          )}
-          <Box className={classes.progressBarWrapper}>
-            <Box className={classes.progressBar}>
-              <Box
-                className={classes.progressBarValue}
-                style={{
-                  width: playedPercentage + '%',
-                  borderRadius: `0px ${
-                    playedPercentage <= 1 ? 10 * playedPercentage + 'px' : '12px'
-                  } ${playedPercentage <= 1 ? 10 * playedPercentage + 'px' : '12px'} 0px`,
-                }}
-              />
-              {/* <input type={'range'} min={0} max={0.99} /> */}
-            </Box>
-            <Text size={'xs'} role={'productive'} className={classes.duration}>
-              {getDuration()}
-            </Text>
           </Box>
-        </Box>
-        {cover ? (
-          <Box className={classes.imageWrapper}>
+          {cover ? (
             <ImageLoader src={cover} height={height} forceImage />
-          </Box>
-        ) : (
-          <Box className={classes.fileIcon}>{icon}</Box>
-        )}
-        <Box className={classes.color} />
+          ) : (
+            <Box className={classes.fileIcon}>{icon}</Box>
+          )}
+          <Box className={classes.color} />
+        </Box>
+        <Box className={classes.titleRow}>
+          <Title order={4} className={classes.title}>
+            {name}
+          </Title>
+          {/* <IconButton size={'xs'} icon={<ExpandDiagonalIcon height={16} width={16} />} /> */}
+          <IconButton
+            icon={
+              !showPlayer ? (
+                <ControlsPlayIcon height={16} width={16} style={{ color: 'white' }} />
+              ) : isPlaying ? (
+                <ControlsPauseIcon height={16} width={16} style={{ color: COLORS.interactive01 }} />
+              ) : (
+                <ControlsPlayIcon height={16} width={16} style={{ color: 'white' }} />
+              )
+            }
+            rounded
+            style={{
+              backgroundColor: !isPlaying && COLORS.interactive01,
+              border: showPlayer && isPlaying && `2px solid ${COLORS.interactive01}`,
+            }}
+            onClick={
+              !showPlayer
+                ? () => {
+                    setShowPlayer(true);
+                    setIsPlaying(true);
+                  }
+                : () => setIsPlaying(!isPlaying)
+            }
+          />
+        </Box>
       </Box>
-      <Box className={classes.titleRow}>
-        <Title order={4} className={classes.title}>
-          {name}
-        </Title>
-        {/* <IconButton size={'xs'} icon={<ExpandDiagonalIcon height={16} width={16} />} /> */}
-        <IconButton
-          icon={
-            !showPlayer ? (
-              <ControlsPlayIcon height={16} width={16} style={{ color: 'white' }} />
-            ) : isPlaying ? (
-              <ControlsPauseIcon height={16} width={16} style={{ color: COLORS.interactive01 }} />
-            ) : (
-              <ControlsPlayIcon height={16} width={16} style={{ color: 'white' }} />
-            )
-          }
-          rounded
-          style={{
-            backgroundColor: !isPlaying && COLORS.interactive01,
-            border: showPlayer && isPlaying && `2px solid ${COLORS.interactive01}`,
-          }}
-          onClick={
-            !showPlayer
-              ? () => {
-                  setShowPlayer(true);
-                  setIsPlaying(true);
-                }
-              : () => setIsPlaying(!isPlaying)
-          }
-        />
-      </Box>
-    </Box>
+    </>
   );
 };
 
