c1da7c11ed91cabd3ca983dd875bd3a046e51c57
Johan Sosa <sosa.johan@gmail.com>
2022-03-03 17:37:58 +0100

    CREATED: HtmlText component UPDATED: HtmlStyles in TextEditor

---
+++ packages/components/src/typography/HtmlText/HtmlText.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Box } from '@mantine/core';
+import { HtmlTextStyles } from './HtmlText.styles';
+import { HTML_TEXT_DEFAULT_PROPS, HTML_TEXT_PROP_TYPES } from './HtmlText.constants';
+
+const HtmlText = ({ children, className, ...props }) => {
+  const { classes, cx } = HtmlTextStyles({}, { name: 'HtmlText' });
+
+  return (
+    <Box dangerouslySetInnerHTML={{ __html: children }} className={cx(className, classes.root)} />
+  );
+};
+
+HtmlText.defaultProps = HTML_TEXT_DEFAULT_PROPS;
+HtmlText.propTypes = HTML_TEXT_PROP_TYPES;
+
+export { HtmlText };
