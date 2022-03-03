1f28f2c9f9f8d9ce6c0cdef648789b4fe63fe395
Albert Araque Casaus <albert.araque.casaus@gmail.com>
2022-03-03 17:58:13 +0100

    (LMVP-174) - TextEditor - ParagraphTool: ParagraphTool

---
+++ packages/editors/src/tool/ParagraphTool/Paragraph.stories.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { TextEditor } from '../../form/TextEditor/TextEditor';
+import { ParagraphTool, PARAGRAPH_TOOL_DEFAULT_PROPS } from './ParagraphTool';
+
+export default {
+  title: 'Atom/Form/ParagraphTool',
+  parameters: {
+    component: ParagraphTool,
+    docs: {
+      // page: mdx,
+    },
+    design: {
+      type: 'figma',
+      // url: 'https://www.figma.com/file/kcSXz3QZFByFDTumNgzPpV/?node-id=2962%3A31342',
+    },
+  },
+  argTypes: {},
+};
+
+const Template = ({ content, ...props }) => {
+  return (
+    <TextEditor content={content}>
+      <ParagraphTool {...props}></ParagraphTool>
+    </TextEditor>
+  );
+};
+
+export const Playground = Template.bind({});
+
+Playground.args = {
+  ...PARAGRAPH_TOOL_DEFAULT_PROPS,
+  content:
+    '<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>',
+};
---
+++ packages/editors/src/tool/ParagraphTool/ParagraphTool.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import { useContext } from 'react';
+import { TextEditorContext } from '../../form/TextEditorProvider';
+import { StarIcon } from '@bubbles-ui/icons/solid';
+import { Button } from '../../form/Button/Button';
+
+export const PARAGRAPH_TOOL_DEFAULT_PROPS = {
+  label: 'Paragraph',
+};
+
+export const PARAGRAPH_TOOL_PROP_TYPES = {
+  label: PropTypes.string,
+};
+
+const ParagraphTool = ({ label, ...props }) => {
+  const { editor } = useContext(TextEditorContext);
+
+  const onClickHandler = () => {
+    editor?.chain().focus().setParagraph().run();
+  };
+
+  return <Button {...props} label={label} icon={<StarIcon />} onClick={onClickHandler}></Button>;
+};
+
+ParagraphTool.defaultProps = PARAGRAPH_TOOL_DEFAULT_PROPS;
+ParagraphTool.propTypes = PARAGRAPH_TOOL_PROP_TYPES;
+
+ParagraphTool.extensions = [];
+
+export { ParagraphTool };
