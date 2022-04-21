c7bd2508ed235686a82aeaeae486299d13b2dd4c
Albert Araque Casaus <albert.araque.casaus@gmail.com>
2022-04-21 16:44:07 +0200

    (LMVP-192) - HeaderBackground: Background with image, color, gradient and blur

---
+++ packages/leemons/src/common/HeaderBackground/HeaderBackground.styles.js
@@ -0,0 +1,54 @@
+import {
+  createStyles,
+  pxToRem,
+  getPaddings,
+  getFontExpressive,
+  getFontProductive,
+} from '@bubbles-ui/components';
+
+export const HeaderBackgroundStyles = createStyles(
+  (theme, { image, color, height, width, withBlur, blur, styles }) => {
+    const isHeightNum = /^\d+$/.test(height);
+    const isWidthNum = /^\d+$/.test(width);
+
+    return {
+      root: {
+        height: isHeightNum ? pxToRem(height) : height,
+        width: isWidthNum ? pxToRem(width) : width,
+        position: 'relative',
+        ...styles,
+      },
+      color: {
+        backgroundColor: color,
+        position: 'absolute',
+        height: '100%',
+        width: '100%',
+        zIndex: 0,
+      },
+      image: {
+        position: 'absolute',
+        height: '100%',
+        width: '100%',
+        backgroundImage: image && `url(${image})`,
+        backgroundSize: 'cover',
+        filter: withBlur && 'blur(2px)',
+        zIndex: 1,
+      },
+      // blur: {
+      //   position: 'absolute',
+      //   height: '100%',
+      //   width: '100%',
+      //   zIndex: 2,
+      //   backdropFilter: `blur(${blur}px)`,
+      //   background: 'rgba(0, 0, 0, 0.5)',
+      // },
+      gradient: {
+        position: 'absolute',
+        height: '100%',
+        width: '100%',
+        background: 'linear-gradient(93.4deg, rgba(0, 0, 0, 0.5) 0%, rgba(0, 0, 0, 0) 33.78%)',
+        zIndex: 3,
+      },
+    };
+  }
+);
---
+++ packages/leemons/src/common/HeaderBackground/index.js
@@ -0,0 +1,2 @@
+export * from './HeaderBackground';
+export * from './HeaderBackground.constants';
