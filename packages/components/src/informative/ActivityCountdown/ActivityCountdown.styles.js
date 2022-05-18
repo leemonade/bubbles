bf4fb1002df74524398d4b6f91b19e4ced09bf12
Albert Araque Casaus <albert.araque.casaus@gmail.com>
2022-05-18 13:26:00 +0200

    (LMVP-208) - ActivityCountdown: Generated and finished

---
+++ packages/components/src/informative/ActivityCountdown/ActivityCountdown.styles.js
@@ -0,0 +1,20 @@
+import { createStyles } from '@mantine/styles';
+import { pxToRem, getPaddings, getFontExpressive, getFontProductive } from '../../theme.mixins';
+
+export const ActivityCountdownStyles = createStyles((theme, { rootStyles }) => {
+  return {
+    root: {
+      display: 'inline-flex',
+      gap: 36,
+      paddingBlock: 18,
+      paddingInline: 26,
+      ...rootStyles,
+    },
+    countdown: {
+      fontSize: 24,
+      lineHeight: '24px',
+      fontWeight: 500,
+      color: theme.colors.text01,
+    },
+  };
+});
