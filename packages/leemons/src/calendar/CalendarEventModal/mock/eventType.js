0b663963729645dfee197e2f70518540a2c189e8
Johan Sosa <sosa.johan@gmail.com>
2022-05-25 08:38:23 +0200

    ADDED: Mock Data to CalendarEventModal

--- packages/leemons/src/calendar/CalendarEventModal/mock/eventType.js
+++ packages/leemons/src/calendar/CalendarEventModal/mock/eventType.js
@@ -1,3 +1,21 @@
+export const EVENT_DATA = {
+  calendar: 'cbc25cf0-7282-4da9-95b2-8a3c4657dc41',
+  created_at: '2022-05-24T15:08:44.000Z',
+  data: { videoLink: 'http://dev.leemons.io' },
+  videoLink: 'http://dev.leemons.io',
+  deleted: 0,
+  deleted_at: null,
+  endDate: '2022-05-19T14:00:23.000Z',
+  id: 'e48701a8-8628-4c1e-80c0-20939805ecef',
+  isAllDay: 0,
+  isPrivate: 0,
+  repeat: 'dont_repeat',
+  startDate: '2022-05-19T13:00:23.000Z',
+  status: 'active',
+  title: 'Work group call for Biology class lorem ipsum',
+  type: 'plugins.calendar.event',
+};
+
 export const eventTypeMock = [
   {
     id: '059d639e-b5bb-4d3d-b400-fcca77086924',
