019d57e6796b7e7639dda81c77ce26253133eace
Jaime Gómez Cimarro <cerberupo@gmail.com>
2022-05-26 15:37:14 +0200

    Export useAccordioState

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -1,4 +1,5 @@
 import SimpleBar from 'simplebar-react';
+
 export { SimpleBar };
 export {
   Grid,
@@ -10,6 +11,7 @@ export {
   SegmentedControl,
   UnstyledButton,
   List,
+  useAccordionState,
 } from '@mantine/core';
 export {
   useId,
