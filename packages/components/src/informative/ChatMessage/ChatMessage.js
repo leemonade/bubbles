98fc10b4ac9604c37ae587013b80d5742e6b6bc6
Jaime Gómez Cimarro <cerberupo@gmail.com>
2022-07-29 14:38:49 +0200

    ChatMessage user now can hidde.

--- packages/components/src/informative/ChatMessage/ChatMessage.js
+++ packages/components/src/informative/ChatMessage/ChatMessage.js
@@ -5,19 +5,20 @@ import { UserDisplayItem } from '../UserDisplayItem';
 import { ChatMessageStyles } from './ChatMessage.styles';
 import { CHAT_MESSAGE_DEFAULT_PROPS, CHAT_MESSAGE_PROP_TYPES } from './ChatMessage.constants';
 
-const ChatMessage = ({ message, user, isOwn, isOnline, locale, ...props }) => {
+const ChatMessage = ({ message, user, isOwn, isOnline, locale, showUser = true, ...props }) => {
   const { classes, cx } = ChatMessageStyles({ isOwn }, { name: 'ChatMessage' });
   return (
     <Box className={classes.root}>
-      <UserDisplayItem
+      {showUser ? <UserDisplayItem
         {...user}
-        size="xs"
+        size='xs'
         layout={isOwn ? 'right' : 'left'}
         state={isOnline ? 'activity' : undefined}
-      />
+      /> : null}
+
       <Box className={classes.messageBox}>
         {message?.type === 'text' && (
-          <Text role="productive" color="primary" size="md">
+          <Text role='productive' color='primary' size='md'>
             {message?.content}
           </Text>
         )}
