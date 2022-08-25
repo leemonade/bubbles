7fc46376924eb15b2dd690eb23d3cd323079a8ae
Jaime Gómez Cimarro <cerberupo@gmail.com>
2022-08-25 14:45:51 +0200

    feat: Limit calendar sub van page selector to 2 if more then print in vertical.

--- packages/leemons/src/calendar/CalendarSubNavFilters/CalendarSubNavFilters.styles.js
+++ packages/leemons/src/calendar/CalendarSubNavFilters/CalendarSubNavFilters.styles.js
@@ -3,17 +3,22 @@ import { createStyles, getFontExpressive } from '@bubbles-ui/components';
 export const CalendarSubNavFiltersStyles = createStyles((theme, {}) => {
   return {
     subNav: {
-      height: '100%',
+      height: '100%'
     },
     switchLabel: {
-      color: theme.colors.text07,
+      color: theme.colors.text07
     },
     segmentRoot: {
       height: '42px',
       padding: 2,
-      backgroundColor: theme.colors.uiBackground05,
+      backgroundColor: theme.colors.uiBackground05
       // paddingLeft: 3,
       // paddingRight: 0,
+    },
+    segmentRoot2: {
+      width: '100%',
+      padding: 2,
+      backgroundColor: theme.colors.uiBackground05
     },
     segmentLabel: {
       ...getFontExpressive(),
@@ -25,24 +30,24 @@ export const CalendarSubNavFiltersStyles = createStyles((theme, {}) => {
       height: '100%',
       margin: 0,
       '&:hover': {
-        color: theme.colors.text07,
-      },
+        color: theme.colors.text07
+      }
     },
     segmentLabelActive: {
-      color: `${theme.colors.text07}!important`,
+      color: `${theme.colors.text07}!important`
     },
     segmentActive: {
       backgroundColor: theme.colors.interactive02d,
       top: 4,
-      left: 4,
+      left: 4
     },
     segmentControl: {
-      border: 'none!important',
+      border: 'none!important'
     },
     icon: {
       img: {
-        filter: 'brightness(0) invert(1)',
-      },
-    },
+        filter: 'brightness(0) invert(1)'
+      }
+    }
   };
 });
