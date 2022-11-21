e367d030ed9f4ac78ae9998829b7ed463eecdc75
MIGUELez11 <miguel.mangel.ez11@gmail.com>
2022-11-21 13:48:15 +0100

    feat(components): New script added to compile tokens
    The script is yarn compile-tokens
    
    refactor(components): Now using tokens-compiled.js instead of json in ThemeProvider

---
+++ packages/components/scripts/compile-tokens.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Generate tokens-compiled.json file
+npx token-transformer ../../tokens.json tokens-compiled.json core,global,component;
+# Create a new tokens-compiled.js file starting with "export default "
+printf "export default " > ./tokens-compiled.js;
+# Insert the tokens-compiled.json into tokens-compiled.js
+cat ./tokens-compiled.json >> ./tokens-compiled.js;
+# Remove the tokens-compiled.json file
+rm ./tokens-compiled.json
