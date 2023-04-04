dc48c34edaf26eecf5161f69edc099afbc6ec271
Johan Sosa <sosa.johan@gmail.com>
2023-04-04 16:16:46 +0200

    fix: Filter all props received in Box component to minimize the logs errors

--- packages/components/src/layout/Box/Box.js
+++ packages/components/src/layout/Box/Box.js
@@ -8,10 +8,56 @@ export const BOX_PROP_TYPES = {
   padding: PropTypes.number,
 };
 
+const MANTINE_PROPS = [
+  'sx', 'styles', 'classNames',
+  'm', 'my', 'mx', 'mt', 'mb', 'ml', 'mr',
+  'p', 'py', 'px', 'pt', 'pb', 'pl', 'pr',
+  'bg', 'c', 'opacity',
+  'ff', 'fz', 'fw', 'lts', 'ta', 'lh', 'fs', 'tt', 'td',
+  'w', 'miw', 'maw', 'h', 'mih', 'mah',
+  'bgsz', 'bgp', 'bgr', 'bga',
+  'pos', 'top', 'left', 'bottom', 'right', 'inset',
+  'display'
+];
+
+const HTML_PROPS = [
+  'id', 
+  'dir',
+  'lang',
+  'role', 
+  'slot',
+  'style',
+  'title',
+  'hidden',
+  'children',
+  'disabled', 
+  'tabIndex',
+  'unstyled',
+  'draggable',
+  'translate',
+  'spellCheck',
+  'contentEditable',
+  'dangerouslySetInnerHTML'
+];
+
 const Box = forwardRef(({ className, noFlex, skipFlex, padding, ...props }, ref) => {
   const { classes, cx } = BoxStyles({ padding }, { name: 'Box' });
 
-  return <MantineBox {...props} ref={ref} className={cx(classes.root, className)} />;
+  const validProps = [
+    ...HTML_PROPS,
+    ...MANTINE_PROPS,
+    ...Object.keys(props).filter(propName => {
+    return propName.startsWith('data-') || propName.startsWith('on') || propName.startsWith('aria-') || propName.startsWith('mantine-');
+  })];
+
+  const filteredProps = Object.keys(props).reduce((result, propName) => {
+    if (validProps.includes(propName)) {
+      result[propName] = props[propName];
+    }
+    return result;
+  }, {});
+
+  return <MantineBox {...filteredProps} ref={ref} className={cx(classes.root, className)} />;
 });
 
 Box.defaultProps = BOX_DEFAULT_PROPS;
