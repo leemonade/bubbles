342d7942641a16ff3954da62a095aceda1c87307
MIGUELez11 <miguel@leemons.io>
2023-04-20 11:58:20 +0200

    fix(LibraryCardFootr): Do not throw if no role is given

--- packages/leemons/src/library/LibraryCardFooter/LibraryCardFooter.js
+++ packages/leemons/src/library/LibraryCardFooter/LibraryCardFooter.js
@@ -65,8 +65,8 @@ const LibraryCardFooter = ({
         fileExtension={fileExtension}
         color={'#636D7D'}
         label={
-          (fileType || variantTitle || variant).charAt(0).toUpperCase() +
-          (fileType || variantTitle || variant).slice(1)
+          (fileType || variantTitle || variant)?.charAt(0)?.toUpperCase() +
+          (fileType || variantTitle || variant)?.slice(1)
         }
         hideExtension
       />
