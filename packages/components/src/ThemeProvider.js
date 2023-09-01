b620aa8abf8e7a864acc73bafe01c33eeab208ff
Jaime Gómez Cimarro <jaime@leemons.io>
2023-09-01 16:06:25 +0200

    feat: Change font.

--- packages/components/src/ThemeProvider.js
+++ packages/components/src/ThemeProvider.js
@@ -69,7 +69,7 @@ const ThemeProvider = ({ children, theme }) => {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;400;500;600&family=Lexend:wght@300;400;500;600&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Albert+Sans:wght@100;300;400;500;600&display=swap"
           rel="stylesheet"
         />
       </Helmet>
