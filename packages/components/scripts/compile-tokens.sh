30a07e3fb790a2502a7045b7f1d64322c88582ea
MIGUELez11 <miguel@leemons.io>
2023-10-19 12:23:37 +0200

    feat: Add merger for old and new figma tokens so no missing tokens are being used

--- packages/components/scripts/compile-tokens.sh
+++ packages/components/scripts/compile-tokens.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+# TODO: Change input tokens to the figma tokens instead of merged ones
+# TODO: once they are fully unified
+
 # Generate tokens-compiled.json file
-npx token-transformer ../../tokens.json tokens-compiled.json core,global,component;
+npx token-transformer ../../merged_tokens.json tokens-compiled.json core,global,component;
 # Create a new tokens-compiled.js file starting with "export default "
 printf "export default " > ./src/tokens.compiled.js;
 # Insert the tokens-compiled.json into tokens-compiled.js
