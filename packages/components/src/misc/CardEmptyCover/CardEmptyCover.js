92addeb7461583fa1d2ba32752a25bb26bb0e690
Johan Sosa <sosa.johan@gmail.com>
2023-12-24 14:40:01 +0100

    feat: TableInput action render, CardEmpty better icons and resolution

--- packages/components/src/misc/CardEmptyCover/CardEmptyCover.js
+++ packages/components/src/misc/CardEmptyCover/CardEmptyCover.js
@@ -1,57 +1,55 @@
 import React, { useRef, memo } from 'react';
-import {
-  AssetAudioIcon,
-  AssetVideoIcon,
-  AssetImageIcon,
-  AssetBookmarkIcon,
-  Box,
-} from '@bubbles-ui/components';
 import { useHTMLToCanvas } from '../../hooks/useHTMLToCanvas';
 import { CardEmptyCoverStyles } from './CardEmptyCover.styles';
 import {
   CARD_EMPTY_COVER_DEFAULT_PROPS,
   CARD_EMPTY_COVER_PROP_TYPES,
 } from './CardEmptyCover.constants';
+import { FileItemDisplay } from '../../informative/FileItemDisplay';
 import { AssetTaskIcon } from '../FileIcon/AssetTaskIcon';
 import { AssetExpressTaskIcon } from '../FileIcon/AssetExpressTaskIcon';
 import { AssetModuleIcon } from '../FileIcon/AssetModuleIcon';
 import { AssetFeedbackIcon } from '../FileIcon/AssetFeedbackIcon';
-import { FileItemDisplay } from '../../informative/FileItemDisplay';
+import { AssetAudioIcon } from '../FileIcon/AssetAudioIcon'
+import { AssetVideoIcon } from '../FileIcon/AssetVideoIcon'
+import { AssetImageIcon } from '../FileIcon/AssetImageIcon'
+import { AssetBookmarkIcon } from '../FileIcon/AssetBookmarkIcon'
 import { AssetDocumentIconSmall } from '../FileIcon/AssetDocumentIcon';
 import { AssetScormIcon } from '../FileIcon/AssetScormIcon';
+import { Box } from '../../layout/Box';
 
 const CardEmptyCover = memo(({ icon, fileType }) => {
   const pairColumnRef = useRef(null);
   const { canvasImage } = useHTMLToCanvas(pairColumnRef, fileType);
   const FileTypeIcon = [
-    { key: 'video', value: <AssetVideoIcon height={24} width={24} color={'#878D96'} /> },
-    { key: 'audio', value: <AssetAudioIcon height={24} width={24} color={'#878D96'} /> },
-    { key: 'image', value: <AssetImageIcon height={24} width={24} color={'#878D96'} /> },
-    { key: 'bookmark', value: <AssetBookmarkIcon height={24} width={24} color={'#878D96'} /> },
-    { key: 'feedback', value: <AssetFeedbackIcon height={24} width={24} color={'#878D96'} /> },
+    { key: 'video', value: <AssetVideoIcon height={18} width={18} color={'#878D96'} /> },
+    { key: 'audio', value: <AssetAudioIcon height={18} width={18} color={'#878D96'} /> },
+    { key: 'image', value: <AssetImageIcon height={18} width={18} color={'#878D96'} /> },
+    { key: 'bookmark', value: <AssetBookmarkIcon height={18} width={18} color={'#878D96'} /> },
+    { key: 'feedback', value: <AssetFeedbackIcon height={18} width={18} color={'#878D96'} /> },
     { key: 'document', value: <AssetDocumentIconSmall height={18} width={18} color={'#878D96'} /> },
-    { key: 'task', value: <AssetTaskIcon height={24} width={24} color={'#878D96'} /> },
+    { key: 'task', value: <AssetTaskIcon height={18} width={18} color={'#878D96'} /> },
     {
       key: 'taskexpress',
-      value: <AssetExpressTaskIcon height={24} width={24} color={'#878D96'} />,
+      value: <AssetExpressTaskIcon height={18} width={18} color={'#878D96'} />,
     },
     {
       key: 'learningpaths.module',
-      value: <AssetModuleIcon height={24} width={24} color={'#878D96'} />,
+      value: <AssetModuleIcon height={18} width={18} color={'#878D96'} />,
     },
     {
       key: 'scorm',
-      value: <AssetScormIcon height={24} width={24} color={'#878D96'} />,
+      value: <AssetScormIcon height={18} width={18} color={'#878D96'} />,
     },
     {
       key: 'file',
-      value: <FileItemDisplay size={18} color={'#878D96'} />,
+      value: <FileItemDisplay size={24} color={'#878D96'} iconStyle={{ backgroundColor: '#f8f9fb' }} />,
     },
   ];
 
   const fileIcon = FileTypeIcon.find(({ key }) => key === fileType);
+  const iconToShow = fileIcon?.value ?? icon;
 
-  const iconToShow = fileIcon?.value || icon;
   const { classes } = CardEmptyCoverStyles();
 
   const pairCol = (
--- packages/components/src/misc/FileIcon/AssetExpressTaskIcon/AssetExpressTaskIcon.js
+++ packages/components/src/misc/FileIcon/AssetExpressTaskIcon/AssetExpressTaskIcon.js
@@ -8,7 +8,7 @@ const AssetExpressTaskIcon = ({ width, height, color }) => (
   <svg
     width={width}
     height={height}
-    viewBox="0 0 24 24"
+    viewBox="0 0 16 18"
     fill="none"
     stroke="currentColor"
     xmlns="http://www.w3.org/2000/svg"
