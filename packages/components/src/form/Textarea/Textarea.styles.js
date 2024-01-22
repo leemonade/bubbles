8e59bb007edc14ec327565b5128ffcdbedfdb708
Jaime Gómez Cimarro <cerberupo@gmail.com>
2024-01-22 11:49:17 +0100

    feat(teatarea): Change styles

--- packages/components/src/form/Textarea/Textarea.styles.js
+++ packages/components/src/form/Textarea/Textarea.styles.js
@@ -1,12 +1,13 @@
 import { createStyles } from '@mantine/styles';
 import { getInputSizes, getInputStyle } from '../mixins/fieldStyles.mixins';
 
-export const TextareaStyles = createStyles((theme, { size }) => {
+export const TextareaStyles = createStyles((theme, { size, textareaStyles = {} }) => {
   const inputTheme = theme.other.input;
   return {
     input: {
       ...getInputSizes(size || 'md', inputTheme.spacing.padding, false),
       ...getInputStyle(inputTheme, theme.other.global),
+      ...textareaStyles,
     },
     wrapper: {
       position: 'relative',
