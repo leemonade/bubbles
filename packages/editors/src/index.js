576179908b4c506567b14ce1d03752538742270e
Borja Leemons <volcanos23@gmail.com>
2024-02-13 16:40:36 +0100

    feat(CCreator): Math extension working

--- packages/editors/src/index.js
+++ packages/editors/src/index.js
@@ -1,6 +1,23 @@
 import { Node, Mark, InputRule } from '@tiptap/core';
-import { ReactNodeViewRenderer, mergeAttributes, NodeViewWrapper } from '@tiptap/react';
+import {
+  ReactNodeViewRenderer,
+  mergeAttributes,
+  NodeViewWrapper,
+  NodeViewContent,
+} from '@tiptap/react';
+
+import { Plugin, PluginKey } from '@tiptap/pm/state';
 
 export * from './form';
 export * from './tool';
-export { Node, Mark, InputRule, ReactNodeViewRenderer, mergeAttributes, NodeViewWrapper };
+export {
+  Node,
+  Mark,
+  InputRule,
+  ReactNodeViewRenderer,
+  mergeAttributes,
+  NodeViewWrapper,
+  NodeViewContent,
+  Plugin,
+  PluginKey,
+};
