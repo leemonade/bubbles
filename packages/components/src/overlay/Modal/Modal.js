0a052332aae5cdba6d4911372b2e7913442451c6
JohanFX <sosa.johan@gmail.com>
2024-06-18 12:44:30 +0200

    feat(modal): Added `padding` prop to change the padding around the modal
    Ticket: task/Client-manager-125
    Reviewed-by: @MIGUELez11
    Refs: #171

--- packages/components/src/overlay/Modal/Modal.js
+++ packages/components/src/overlay/Modal/Modal.js
@@ -3,8 +3,8 @@ import { Modal as MantineModal } from '@mantine/core';
 import { ModalStyles } from './Modal.styles';
 import { MODAL_DEFAULT_PROPS, MODAL_PROP_TYPES } from './Modal.constants';
 
-const Modal = forwardRef(({ centerTitle, empty, size, ...props }, ref) => {
-  const { classes, cx } = ModalStyles({ empty, centerTitle, size }, { name: 'Modal' });
+const Modal = forwardRef(({ centerTitle, empty, size, padding, ...props }, ref) => {
+  const { classes, cx } = ModalStyles({ empty, centerTitle, size, padding }, { name: 'Modal' });
 
   return <MantineModal {...props} size={size} ref={ref} classNames={classes} />;
 });
