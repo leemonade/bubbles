09bf53bd2d0195b4286aa12a3f2339c892af3bf3
Fernando Marín Sánchez <46426271+fermarinsanchez@users.noreply.github.com>
2024-06-25 14:54:05 +0200

    feat(components): New size for AvatarSubject. Add name prop. If don't have iconm show initials.

--- packages/components/src/misc/AvatarSubject/AvatarSubject.js
+++ packages/components/src/misc/AvatarSubject/AvatarSubject.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Box } from '@mantine/core';
+import { Box, Stack } from '@mantine/core';
 import { AvatarSubjectStyles } from './AvatarSubject.styles';
 import { AVATAR_SUBJECT_DEFAULT_PROPS, AVATAR_SUBJECT_PROP_TYPES } from './AvatarSubject.constants';
 import { MultiSubjectIcon } from './MultiSubjectIcon/MultiSubjectIcon';
 import { ImageLoader } from '../ImageLoader';
+import { Text } from '../../typography';
 
-const AvatarSubject = ({ color, icon, size, altText, isMultiSubject }) => {
+const AvatarSubject = ({ color, icon, size, isMultiSubject, name }) => {
   const { classes } = AvatarSubjectStyles({ size });
   const iconToShow = icon || null;
   const handleColor = isMultiSubject ? '#878D96' : color;
@@ -15,22 +16,38 @@ const AvatarSubject = ({ color, icon, size, altText, isMultiSubject }) => {
     md: { height: '12px' },
     lg: { height: '18px' },
     xlg: { height: '30px' },
+    xxlg: { height: '48px' },
   };
+
+  const texts = name ? name.split(' ') : [];
+  const initials =
+    texts.length >= 1
+      ? `${texts[0][0].toUpperCase()}${texts[1] ? texts[1][0].toUpperCase() : ''}`
+      : '';
+
+  const IconOrInitalsComp = icon ? (
+    <ImageLoader
+      forceImage
+      height={handleSize[size].height}
+      imageStyles={handleSize[size].height}
+      src={iconToShow}
+    />
+  ) : (
+    <Text size="lg" className={classes.text}>
+      {name ? initials : ''}
+    </Text>
+  );
 
   return (
     <Box className={classes.bubble} style={{ backgroundColor: handleColor }}>
       {isMultiSubject ? (
         <MultiSubjectIcon width={handleSize[size].height} height={handleSize[size].height} />
       ) : (
-        !!iconToShow && (
-          <ImageLoader
-            forceImage
-            height={handleSize[size].height}
-            imageStyles={handleSize[size].height}
-            src={iconToShow}
-            alt={altText}
-          />
-        )
+        <Stack>
+          <Text size="lg" className={classes.text}>
+            {IconOrInitalsComp}
+          </Text>
+        </Stack>
       )}
     </Box>
   );
