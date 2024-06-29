363c3707b7f0ca19400c2d22e71028dc57d8f971
JohanFX <sosa.johan@gmail.com>
2024-06-29 22:02:57 +0200

    feat(kanban): add new item functionality to the first column
    - Added showNewOnFirstColumn and newItemLabel props to Kanban component.
    - Updated Kanban styles to support new item addition.
    - Modified Column component to display a new item button when the first column is empty.
    - Updated mock data to include additional tasks.
    Ticket: task/Dashboards-135
    Reviewed-by: @MIGUELez11
    Refs: #176

--- packages/components/src/informative/Kanban/Kanban.styles.js
+++ packages/components/src/informative/Kanban/Kanban.styles.js
@@ -1,58 +1,61 @@
 import { createStyles } from '@mantine/styles';
 
-export const KanbanStyles = createStyles((theme, {}) => {
-  return {
-    root: {
-      overflow: 'auto',
-      whiteSpace: 'nowrap',
-      height: '100%',
-      maxHeight: '100%',
-      padding: theme.spacing[2],
-      backgroundColor: '#F8F9FB',
-      gap: theme.spacing[2],
-      display: 'flex',
+const KanbanStyles = createStyles((theme) => ({
+  root: {
+    display: 'flex',
+    overflow: 'auto',
+    whiteSpace: 'nowrap',
+    height: '100%',
+    maxHeight: '100%',
+    gap: theme.spacing[2],
+    alignItems: 'stretch',
+    padding: theme.spacing[2],
+    backgroundColor: '#F8F9FB',
+  },
+  column: {
+    display: 'inline-flex',
+    position: 'relative',
+    width: '300px',
+    minWidth: '300px',
+    flexDirection: 'column',
+    backgroundColor: '#F2F4F8',
+    borderRadius: theme.spacing[1],
+  },
+  columnHeader: {
+    paddingLeft: theme.spacing[4],
+    paddingRight: theme.spacing[4],
+    paddingTop: theme.spacing[3],
+    color: '#4D5358',
+    fontSize: '16px',
+    fontWeight: 600,
+    lineHeight: '24px',
+    fontFamily: 'Albert Sans',
+    display: 'flex',
+    gap: theme.spacing[2],
+    alignItems: 'center',
+  },
+  scroll: {
+    padding: theme.spacing[2],
+  },
+  listDraggingOver: {
+    backgroundColor: '#EDEFF5',
+    borderRadius: 4,
+  },
+  list: {
+    minHeight: '100px',
+  },
+  iconBig: {
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
+    zIndex: 0,
+    img: {
+      filter: 'brightness(0) invert(1)',
     },
-    column: {
-      height: '100%',
-      display: 'inline-flex',
-      position: 'relative',
-      width: '300px',
-      minWidth: '300px',
-      flexDirection: 'column',
-      backgroundColor: '#F2F4F8',
-      borderRadius: theme.spacing[1],
-    },
-    columnHeader: {
-      paddingLeft: theme.spacing[4],
-      paddingRight: theme.spacing[4],
-      paddingTop: theme.spacing[3],
-      color: '#4D5358',
-      fontSize: '16px',
-      fontWeight: 600,
-      lineHeight: '24px',
-      fontFamily: 'Albert Sans',
-      display: 'flex',
-      gap: theme.spacing[2],
-      alignItems: 'center',
-    },
-    scroll: {
-      padding: theme.spacing[2],
-    },
-    listDraggingOver: {
-      backgroundColor: '#EDEFF5',
-      borderRadius: 4,
-    },
-    list: {
-      minHeight: '100px',
-    },
-    iconBig: {
-      position: 'absolute',
-      bottom: 20,
-      left: 20,
-      zIndex: 0,
-      img: {
-        filter: 'brightness(0) invert(1)',
-      },
-    },
-  };
-});
+  },
+  newItem: {
+    padding: theme.spacing[2],
+  },
+}));
+
+export { KanbanStyles };
