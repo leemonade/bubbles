3cbf31a95d2d0b599659a6c8494a859082f84982
Fernando Marín Sánchez <46426271+fermarinsanchez@users.noreply.github.com>
2024-07-17 23:45:46 +0200

    feat(TextEditorInput): streamline toolbar configuration and introduce custom editor styling
    - Simplified toolbar configuration by removing deprecated constants and introducing a single 'toolbars' prop.
    - Removed the need for specifying different toolbars based on presets ('textArea'), instead using the 'toolbars' prop to directly control the behavior.
    
    ---------
    Ticket: issue/Test-253
    Co-authored-by: @johan-fx 
    Reviewed-by: @johan-fx 
    Ref: #180

--- packages/editors/src/form/TextEditorInput/TextEditorInput.constants.js
+++ packages/editors/src/form/TextEditorInput/TextEditorInput.constants.js
@@ -11,6 +11,17 @@ export const TEXT_EDITOR_DEFAULT_TOOLBARS = {
   link: true,
 };
 
+export const TEXT_EDITOR_TEXTAREA_TOOLBARS = {
+  style: true,
+  heading: false,
+  align: true,
+  list: true,
+  history: true,
+  color: true,
+  formulation: false,
+  link: true,
+};
+
 export const TEXT_EDITOR_INPUT_DEFAULT_PROPS = {
   placeholder: '',
   toolbars: TEXT_EDITOR_DEFAULT_TOOLBARS,
