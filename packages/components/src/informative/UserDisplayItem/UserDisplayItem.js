1042f3227bed4643bf7a5cbc5f86fab45dab1d3e
Johan Sosa <sosa.johan@gmail.com>
2024-09-13 18:52:03 +0200

    fix(UserDisplayItem): Use Highlight from bubble instead of Mantine

--- packages/components/src/informative/UserDisplayItem/UserDisplayItem.js
+++ packages/components/src/informative/UserDisplayItem/UserDisplayItem.js
@@ -2,7 +2,7 @@ import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { isEmpty } from 'lodash';
 import { PluginComunicaIcon, AlertWarningTriangleIcon, BlockIcon } from '@bubbles-ui/icons/solid/';
-import { Highlight } from '@mantine/core';
+import { Highlight } from '../../typography/Highlight';
 import { Box } from '../../layout/Box';
 import { Avatar } from '../Avatar/Avatar';
 import { Text, TextClamp, TEXT_ROLES } from '../../typography';
@@ -20,6 +20,7 @@ export const USER_DISPLAY_ITEM_DEFAULT_PROPS = {
   layout: 'left',
   noBreak: false,
   textRole: 'expressive',
+  highlight: '',
 };
 export const USER_DISPLAY_ITEM_SEVERITIES = ['warning', 'error'];
 
@@ -38,6 +39,7 @@ export const USER_DISPLAY_ITEM_PROP_TYPES = {
   textRole: PropTypes.oneOf(TEXT_ROLES),
   onChat: PropTypes.func,
   noBreak: PropTypes.bool,
+  highlight: PropTypes.string,
 };
 
 const UserDisplayItem = (properties) => {
