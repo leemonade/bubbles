a836b5ca52dba758cdd7962a228ad9948acb0ac9
Fernando Marín Sánchez <46426271+fermarinsanchez@users.noreply.github.com>
2024-09-27 11:42:10 +0200

    feat(Chips): add two new props to truncate to n lines the chip text content

--- packages/components/src/informative/Chip/Chip.constants.js
+++ packages/components/src/informative/Chip/Chip.constants.js
@@ -4,6 +4,10 @@ export const CHIP_PROP_TYPES = {
   subject: propTypes.string.isRequired,
   isHidden: propTypes.bool.isRequired,
   isCollisionDetected: propTypes.bool,
+  truncate: propTypes.bool,
+  truncateLines: propTypes.number,
 };
 
-export const CHIP_DEFAULT_PROPS = {};
+export const CHIP_DEFAULT_PROPS = {
+  truncate: false,
+};
