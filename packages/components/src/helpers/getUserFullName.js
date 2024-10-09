bc5e0f9e81a5d085537c58471fdedce951ede50f
Miguel Ángel Florido Cifuentes <miguel@leemons.io>
2024-10-09 15:59:29 +0200

    refactor(components): getUserFullName now trims the getUserFullName
    The previous implementation was not trimming individual results and was
    developed without following the linter rules, this PR fixes that.

--- packages/components/src/helpers/getUserFullName.js
+++ packages/components/src/helpers/getUserFullName.js
@@ -1,7 +1,12 @@
 function getUserFullName(user) {
-  return `${user.secondSurname ? `${user.secondSurname} ` : ''}${
-    user.surnames ? user.surnames : ''
-  }${user.secondSurname || user.surnames ? ', ' : ''}${user.name}`;
+  const name = user.name?.trim() ?? null;
+  const surname = user.surnames?.trim() ?? null;
+  const secondSurname = user.secondSurname?.trim() ?? null;
+
+  const surnames = [secondSurname, surname].filter(Boolean).join(' ');
+  const fullName = [surnames, name].filter(Boolean).join(', ');
+
+  return fullName.trim();
 }
 
 export { getUserFullName };
