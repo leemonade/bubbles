a90d04992555682299216f48196d71bbda272e2f
Fernando Marín Sánchez <46426271+fermarinsanchez@users.noreply.github.com>
2024-10-25 13:58:55 +0200

    fix(components): Tabs. Update Menu component to v5 sintax. Adjust useVisibleRange hook and style Dropdown button. fix(components): IconButton. Add cx to merge static and dynamic classNames

--- packages/components/src/navigation/Tabs/TabNavList/Dropdown.styles.js
+++ packages/components/src/navigation/Tabs/TabNavList/Dropdown.styles.js
@@ -1,12 +1,26 @@
 import { createStyles } from '@mantine/styles';
 
-export const DropdownStyles = createStyles((theme, {}, getRef) => {
-  return {
-    root: {
-      paddingLeft: theme.spacing['3'],
-      display: 'flex',
-      alignItems: 'center',
-      // paddingBottom: theme.spacing['2'],
+const DropdownStyles = createStyles((theme, {}, getRef) => ({
+  root: {
+    paddingLeft: theme.spacing['3'],
+    display: 'flex',
+    alignItems: 'center',
+    zIndex: 1000,
+    // paddingBottom: theme.spacing['2'],
+  },
+  item: {
+    '&:hover': {
+      backgroundColor: theme.other.button.content.color.terciary.default,
     },
-  };
-});
+  },
+  buttonIcon: {
+    '&:hover': {
+      backgroundColor: theme.other.buttonIcon.background.color.ghost.hover,
+      '& > svg': {
+        color: theme.other.buttonIcon.background.color.secondary.down,
+      },
+    },
+  },
+}));
+
+export { DropdownStyles };
