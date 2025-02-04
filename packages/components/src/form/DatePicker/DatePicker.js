8c54a5f8cca4226040d2dff7efa64b5ed64f6fa5
paola-p <>
2025-02-04 14:12:03 +0100

    feat(components): Set a default initialMonth for DatePicker components

--- packages/components/src/form/DatePicker/DatePicker.js
+++ packages/components/src/form/DatePicker/DatePicker.js
@@ -1,4 +1,4 @@
-import React, { forwardRef, useEffect, useState } from 'react';
+import React, { forwardRef, useEffect, useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
 import { isEmpty, isFunction, isNil } from 'lodash';
 import {
@@ -87,6 +87,7 @@ export const DATE_PICKER_PROP_TYPES = {
   autoComplete: PropTypes.string,
   readOnly: PropTypes.bool,
   clearButtonLabel: PropTypes.string,
+  initialMonth: PropTypes.instanceOf(Date),
 };
 
 function TimeInput({ onChange, size, ...props }) {
@@ -125,6 +126,7 @@ const DatePicker = forwardRef(
       readOnly,
       style,
       clearable,
+      initialMonth,
       ...props
     },
     ref,
@@ -177,6 +179,19 @@ const DatePicker = forwardRef(
       };
     }, [locale]);
 
+    const defaultInitialMonth = useMemo(() => {
+      const today = dayjs();
+
+      if (props?.minDate && dayjs(props?.minDate).isAfter(today)) {
+        return dayjs(props?.minDate).toDate();
+      }
+      if (props?.maxDate && dayjs(props?.maxDate).isBefore(today)) {
+        return dayjs(props?.maxDate).toDate();
+      }
+
+      return today.toDate();
+    }, [props?.minDate, props?.maxDate]);
+
     return (
       <InputWrapper
         uuid={uuid}
@@ -206,6 +221,7 @@ const DatePicker = forwardRef(
             <Comp
               {...props}
               {...compProps}
+              initialMonth={initialMonth || defaultInitialMonth}
               clearable={clearable}
               withinPortal
               placeHolder={inputFormat}
